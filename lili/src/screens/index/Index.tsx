73652c1bdc2a71489c698f433199798761faee3f
elenaferfor <elena.fer.for@gmail.com>
2026-05-20 10:18:08 +0200

    Corregir miniaturas de libros en index y perfil

--- lili/src/screens/index/Index.tsx
+++ lili/src/screens/index/Index.tsx
@@ -2,10 +2,12 @@ import "./Index.css"
 import SectionSinGet from "../../components/section/SectionSinGet.tsx";
 import {useUsuarioLibrosLista} from "../../hooks/useUsuarioLibro.tsx";
 import {Layout} from "../Layout.tsx";
+import {usePrestamos} from "../../hooks/usePrestamos.tsx";
 
 const Index = () => {
 
     const { data: libros, isLoading: librosIsLoading } = useUsuarioLibrosLista();
+    const { data: prestamos } = usePrestamos();
     
     const deseos = libros?.filter(l => l.categorias_detalle.some(c => c.nombre === "Deseos"));
     
@@ -13,8 +15,8 @@ const Index = () => {
         <div className="contenido">
             <div className="migas">Biblioteca</div>
             <div className="secciones">
-                <SectionSinGet titulo={"Últimos añadidos"} listaLibros={libros?.slice(0, 15)} isLoading={librosIsLoading}/>
-                <SectionSinGet titulo={"Lista de deseos"} listaLibros={deseos?.slice(0, 15)} isLoading={librosIsLoading}/>
+                <SectionSinGet titulo={"Últimos añadidos"} listaLibros={libros?.slice(0, 15)} isLoading={librosIsLoading} prestamos={prestamos}/>
+                <SectionSinGet titulo={"Lista de deseos"} listaLibros={deseos?.slice(0, 15)} isLoading={librosIsLoading} prestamos={prestamos}/>
                 {/*
                 Añadir cuando funcione con Open Library
                 <Section titulo={"Recomendados para ti"} filtroBusqueda={""}/>
